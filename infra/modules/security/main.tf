ccd4164fac4826d45b5e670adb4f8d2289c04480
HirokiIshibashi <ishibashi.6122@icloud.com>
2026-09-03 17:57:50 +0900

    Use inline app security group egress rules

--- infra/modules/security/main.tf
+++ infra/modules/security/main.tf
@@ -28,6 +28,7 @@ resource "aws_security_group" "app" {
   name        = "${var.project_name}-app-sg${var.name_suffix_physical}"
   description = "Application instances, no SSH"
   vpc_id      = var.vpc_id
+
   ingress {
     description     = "Apache from ALB"
     from_port       = 80
@@ -42,6 +43,31 @@ resource "aws_security_group" "app" {
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
   }
+
+  egress {
+    description = "HTTP for packages"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "HTTPS and AWS APIs"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description     = "Application to RDS"
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.db.id]
+  }
+
   tags = {
     Name = "${var.project_name}-app-sg${var.name_suffix_tag}"
   }
@@ -70,31 +96,4 @@ resource "aws_vpc_security_group_egress_rule" "alb_to_app_http" {
   to_port                      = 80
   ip_protocol                  = "tcp"
   description                  = "ALB to Apache"
-}
-
-resource "aws_vpc_security_group_egress_rule" "app_http" {
-  security_group_id = aws_security_group.app.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-  description       = "HTTP for packages"
-}
-
-resource "aws_vpc_security_group_egress_rule" "app_https" {
-  security_group_id = aws_security_group.app.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-  description       = "HTTPS and AWS APIs"
-}
-
-resource "aws_vpc_security_group_egress_rule" "app_to_db" {
-  security_group_id            = aws_security_group.app.id
-  referenced_security_group_id = aws_security_group.db.id
-  from_port                    = 3306
-  to_port                      = 3306
-  ip_protocol                  = "tcp"
-  description                  = "Application to RDS"
 }
